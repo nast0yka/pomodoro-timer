d02e28f64a10e43e345f45ebd7ffa3e212e4c05c
nast0yka <anse.soroka@gmail.com>
2023-01-11 22:26:07 +0300

    корректировка

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
 let btnStart = document.querySelector("#start");
 let timer = document.querySelector("#pomodoro-time");
 
+let timerId;
 
-function funStart() {
-
-    if (btnStart === "start") {
+btnStart.addEventListener("click", () => {
+    if (btnStart.textContent === "start") {
         btnStart.textContent = "stop";
-
-        let timerID = setInterval(() => {
-            let timerID2 = setInterval(() => {
-                let minutes = parseInt(timer.textContent.split(":")[0]);
-                minutes--;
-
-                if (minutes < 0) {
-                    clearInterval(timerID2);
-                    return minutes;
-                }
-                return minutes;
-            }, 5000);
+        timerId = setInterval(() => {
 
             let seconds = parseInt(timer.textContent.split(":")[1]);
             seconds--;
+            let minutes = parseInt(timer.textContent.split(":")[0]);
+            minutes--;
 
             timer.textContent = `${minutes}:${seconds}`;
 
             if (seconds <= 9) {
                 timer.textContent = `${minutes}:0${seconds}`;
             }
-
             if (seconds <= 0) {
                 return seconds = 60;
+            }
+            if (minutes < 0) {
+                clearInterval(timerID);
+                return minutes;
+            }
+            return minutes;
+            if (minutes === 0 || seconds === 0) {
+                timer.textContent = `25:00`;
+                btnStart.textContent = "start"
             }
 
         }, 1000);
     }
-
-    if (btnStart === "stop") {
-        clearInterval(timerID);
+    if (btnStart.textContent === "stop") {
+        clearInterval(timerId);
+        btnStart.textContent = "start";
         return timer;
-    }
-    if (minutes === 0 || seconds === 0) {
-        timer.textContent = `25:00`;
-        btnStart.textContent = "start"
-    }
+    };
 
-
-}
-
-btnStart.addEventListener('click', funStart(timer));
+});
