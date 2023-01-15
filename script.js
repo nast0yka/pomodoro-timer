7c6ae001844697bb4c2168346082d656c2631b7b
nast0yka <anse.soroka@gmail.com>
2023-01-15 09:35:17 +0300

    решение
    по мотивам Полининого решения

--- script.js
+++ script.js
@@ -6,37 +6,33 @@ let timerId;
 btnStart.addEventListener("click", () => {
     if (btnStart.textContent === "start") {
         btnStart.textContent = "stop";
+        let seconds = parseInt(timer.textContent.split(":")[1]);
+        let minutes = parseInt(timer.textContent.split(":")[0]);
         timerId = setInterval(() => {
 
-            let seconds = parseInt(timer.textContent.split(":")[1]);
-            seconds--;
-            let minutes = parseInt(timer.textContent.split(":")[0]);
-            minutes--;
-
-            timer.textContent = `${minutes}:${seconds}`;
-
-            if (seconds <= 9) {
-                timer.textContent = `${minutes}:0${seconds}`;
-            }
-            if (seconds <= 0) {
-                return seconds = 60;
-            }
-            if (minutes < 0) {
-                clearInterval(timerID);
-                return minutes;
-            }
-            return minutes;
-            if (minutes === 0 || seconds === 0) {
+            if (seconds > 0) {
+                seconds--;
+            } else if (minutes > 0 && seconds === 0) {
+                minutes--;
+                seconds = 59;
+            } else if (minutes === 0 || seconds === 0) {
                 timer.textContent = `25:00`;
-                btnStart.textContent = "start"
+                btnStart.textContent = "start";
+                clearInterval(timerId);
             }
+
+            function format(val) {
+                if (val < 10) {
+                    return `0${val}`;
+                }
+                return val;
+            }
+            timer.textContent = `${format(minutes)}:${format(seconds)}`;
 
         }, 1000);
-    }
-    if (btnStart.textContent === "stop") {
+    } else {
         clearInterval(timerId);
         btnStart.textContent = "start";
-        return timer;
     };
 
 });
